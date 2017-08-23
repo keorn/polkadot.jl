32cf89d16a53facf7a0c5a16b0cdd42136459723
keorn <pczaban@gmail.com>
2017-08-23 13:42:53 +0100

    add a readme

---
+++ README.md
@@ -0,0 +1,8 @@
+Requires Julia 0.6
+
+Not a package yet.
+
+```
+./setup.jl
+./run.jl
+```
--- setup.jl
+++ setup.jl
@@ -1,2 +1,5 @@
+#!/usr/bin/env julia
+
+println("Getting packages...")
 [Pkg.add(req) for req in ["SimJulia", "Distributions", "DataStructures"]]
 [Pkg.clone(req) for req in ["git@github.com:c42f/FastClosures.jl.git", "git@github.com:c42f/MicroLogging.jl.git"]]
